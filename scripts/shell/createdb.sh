aedf4896f11a7eb9eb8f7296c148c3b0329df6d7
Yogesh Jadhav <yogeshjadhav96@gmail.com>
2017-06-09 11:58:43 +0530

    Create createdb.sh

---
+++ scripts/shell/createdb.sh
@@ -0,0 +1,17 @@
+######################################################################################################
+#	File         : database-create.sh
+#	Description  : This script will create a database in the local host
+#	Author	     : T S Pradeep Kumar
+#	Date		     : 11/15/2016
+# Version		   : V1.0
+# GitHub Repo  : sysgain/MarkLogic
+######################################################################################################
+
+#!/bin/bash
+
+hostname=$1
+username=$2
+password=$3
+dbname=$4
+curl -X POST  --anyauth -u $username:$password --header "Content-Type:application/json" \
+  -d '{"database-name":"'"$dbname"'"}' http://$hostname:8002/manage/v2/databases
