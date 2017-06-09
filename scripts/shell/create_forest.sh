f5919f294b27eab234e500685251d303e1455b88
Yogesh Jadhav <yogeshjadhav96@gmail.com>
2017-06-09 12:00:08 +0530

    Create create_forest.sh

---
+++ scripts/shell/create_forest.sh
@@ -0,0 +1,23 @@
+######################################################################################################
+#	File          : forest-create.sh
+#	Description   : This script will create a forest in the local host
+#	Author	      : T S Pradeep Kumar
+#	Date		      : 11/15/2016
+# Version		    : V1.0
+# GitHub Repo   : sysgain/MarkLogic
+######################################################################################################
+
+#!/bin/bash
+
+username=$1
+password=$2
+forestname=$3
+databasename=$4
+localhostname=`hostname`
+
+MLHOSTNAME=`curl --anyauth --user $username:$password -X GET \
+-i -H "Content-type: application/json" http://localhost:8002/manage/v2/hosts | grep "<nameref>" | grep "$localhostname" | sed s/\<nameref\>// | sed s/\<.nameref\>// | tr -s ' ' '#' | sed s/#//`
+
+curl --anyauth --user $username:$password -X POST \
+-d '{"forest-name":"'"$forestname"'", "host": "'"$MLHOSTNAME"'", "database": "'"$databasename"'"}' \
+-i -H "Content-type: application/json" http://localhost:8002/manage/v2/forests
