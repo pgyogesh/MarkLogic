e557cc02188aa9ae8d1175989db2522611cd5771
Yogesh Jadhav <yogeshjadhav96@gmail.com>
2017-06-12 15:08:53 +0530

    Create AddPermission.js

---
+++ scripts/JavaScript/AddPermission.js
@@ -0,0 +1,18 @@
+/*
+  Update the documents in the database with appropriate permissions.
+*/
+
+declareUpdate();
+
+if (xdmp.databaseName(xdmp.database()) == "Documents") // To check if DB is correct
+{
+  xdmp.documentAddPermissions("/uri/of/document.json", [xdmp.permission("rolename1", "read"), xdmp.permission("rolename2", "update")]);
+}
+else 
+{
+  "Please select the Documents database from the Content Source menu and run this script again." 
+} 
+
+/*
+Permisions can be "read","insert","update","execute"
+*/
