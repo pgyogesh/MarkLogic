1201a95d9fdedfc4ac971596ae6c98fa7ca1c116
pgyogesh <yogeshjadhav96@gmail.com>
2017-07-16 22:56:57 +0530

    Modified is_ml_running.py

--- scripts/python/is_ml_running.py
+++ scripts/python/is_ml_running.py
@@ -3,10 +3,5 @@ try:
     ping_ml=requests.get('http://localhost:7997')
     if ping_ml.status_code == 200:
         print("MarkLogic Server is running")
-except Exception as e:
-    print(e)
+except Exception:
     print("MarkLogic Server in not running")
-# if ping_ml.status_code == 200:
-#     print("MarkLogic Server is running")
-# else:
-#     print("MarkLogic Server is not running")
